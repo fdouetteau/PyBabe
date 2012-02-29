5167f7492714c0181febbf749f409b7fd0e65f15
Florian Douetteau <florian@douetteau.net>
2012-03-01 00:37:15 +0100

    Refactor method calls

--- pybabe/__init__.py
+++ pybabe/__init__.py
@@ -3,8 +3,12 @@ import csv
 from collections import namedtuple
 import itertools
 
+
+
+
+
 class Babe(object):
-    def pull(self, name, resource, format=None, **kwargs):
+    def pull(self, resource, name, format=None, **kwargs):
         ## Open File
         stream = None
         if hasattr(resource,'read'): 
@@ -17,23 +21,12 @@ class Babe(object):
             dialect = csv.Sniffer().sniff(sniff_read)
         except:
             raise Exception ()
-        r = csv.reader(stream, dialect)
-        names  = r.next()
-        names = [s.strip() for s in names] # Normalize names
-        metainfo = MetaInfo()
-        metainfo.dialect = dialect
-        metainfo.names = names
-        t = namedtuple(name, names)
-        yield metainfo
-        for row in r:
-            yield t._make(row)
-            
-    def map(self, f, column,  stream):
-        return itertools.imap(
-            lambda elt : elt._replace(**{column : f(getattr(elt, column))}) if not isinstance(elt, MetaInfo) else elt,
-            stream)
+        return CSVPull(name, stream, dialect)
+  
+    def map(self, column,  f ):
+        return Map(f, column, self)
         
-    def push(self, resource, stream, format=None, **kwards):
+    def push(self, resource, format=None, **kwards):
         metainfo = None
         writer = None
         if hasattr(resource, 'write'): 
@@ -42,7 +35,7 @@ class Babe(object):
             outstream = open(resource, 'wb')
         else:
             raise Exception()
-        for k in stream: 
+        for k in self: 
             if isinstance(k, MetaInfo):
                 metainfo = k
                 writer = csv.writer(outstream, metainfo.dialect)
@@ -50,13 +43,39 @@ class Babe(object):
             else:
                 writer.writerow(list(k))
                 
+class Map(Babe):
+    def __init__(self, f, column, stream):
+        self.f = f
+        self.column = column 
+        self.stream = stream 
+    def __iter__(self):
+        return itertools.imap(
+               lambda elt : elt._replace(**{self.column : self.f(getattr(elt, self.column))}) if not isinstance(elt, MetaInfo) else elt,
+               self.stream)
+               
+class CSVPull(Babe):
+    def __init__(self, name, stream, dialect):
+        self.name = name
+        self.stream = stream
+        self.dialect = dialect
+    def __iter__(self):
+        r = csv.reader(self.stream, self.dialect)
+        names  = r.next()
+        names = [s.strip() for s in names] # Normalize names
+        metainfo = MetaInfo()
+        metainfo.dialect = self.dialect
+        metainfo.names = names
+        t = namedtuple(self.name, names)
+        yield metainfo
+        for row in r:
+            yield t._make(row)
         
 class MetaInfo(object): 
     pass
     
 if __name__ == "__main__": 
     babe = Babe()
-    babe.push('test2.csv', babe.map(lambda x : int(x) + 10, 'foo',  babe.pull('Test', 'test.csv')))
+    babe.pull('../tests/test.csv', name='Test').map('foo', lambda x : int(x) + 30).push('../tests/test2.csv')
     
         
         
