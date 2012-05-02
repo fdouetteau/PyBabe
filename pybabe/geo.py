82c9d0056f864821be94ae56455b7327f31c869c
Florian Douetteau <florian_github@douetteau.net>
2012-05-02 09:57:48 +0200

    Changed default pathes to lookup Geo databases

--- pybabe/geo.py
+++ pybabe/geo.py
@@ -5,14 +5,14 @@ import os
 
 gic = None
 
-def geoip_country_code(stream, field="ip", country_code="country_code", geoip_file = None): 
-    """"
-Add a 'country_code' field from IP address in field "IP"
-    """
-    global gic
-    if gic == None:
+
+def get_gic(): 
+    global gic 
+    if gic == None: 
         from pygeoip import GeoIP
-        if os.path.exists("/usr/local/share/GeoIP/GeoLiteCity.dat"):
+        if os.path.exists('/usr/share/GeoIP/GeoIP.dat'): 
+            default = "/usr/share/GeoIP/GeoIP.dat"
+        elif os.path.exists("/usr/local/share/GeoIP/GeoLiteCity.dat"):
             default = "/usr/local/share/GeoIP/GeoLiteCity.dat"
         elif os.path.exists("/usr/local/var/lib/GeoLiteCity.dat"):
             default = "/usr/local/var/lib/GeoLiteCity.dat" 
@@ -20,6 +20,14 @@ Add a 'country_code' field from IP address in field "IP"
             default = None
         filename = BabeBase.get_config_with_env('geoip', 'GEOIP_FILE', {}, default)
         gic = GeoIP(filename)
+    return gic 
+
+
+def geoip_country_code(stream, field="ip", country_code="country_code", geoip_file = None): 
+    """"
+Add a 'country_code' field from IP address in field "IP"
+    """
+    gic = get_gic()
     for r in stream:
         if isinstance(r, StreamHeader):
             header = r.insert(typename=None, fields=[country_code])
