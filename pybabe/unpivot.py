60560d8474262f9c07620e97c9c6838f961d3620
Florian Douetteau <florian_github@douetteau.net>
2012-09-10 15:21:20 +0200

    Handle field name where normalization differ from original name

--- pybabe/unpivot.py
+++ pybabe/unpivot.py
@@ -11,8 +11,8 @@ def unpivot(stream, common_fields, unpivot_name_field, unpivot_value_field):
 		elif isinstance(row, StreamMeta):
 			yield row
 		else:
-			commons = [getattr(row, f) for f in common_fields]
+			commons = [getattr(row, StreamHeader.keynormalize(f)) for f in common_fields]
 			for field in other_fields:
-				yield header.t._make(commons + [field, getattr(row, field)])
+				yield header.t._make(commons + [field, getattr(row, StreamHeader.keynormalize(field))])
 
 Babe.register('unpivot', unpivot)
