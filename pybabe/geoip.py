440dd2fd7d0286cf9fd0fac252e7e197f70f8a04
Florian Douetteau <florian_github@douetteau.net>
2012-11-15 11:27:57 +0100

    Added detailed geoip

--- pybabe/geoip.py
+++ pybabe/geoip.py
@@ -23,6 +23,35 @@ def get_gic():
     return gic
 
 
+def geoip(stream, field="ip", ip_blacklist=set(), country_code="country_code", region_name="region_name", city="city", latitude="latitude", longitude="longitude", ignore_error=True):
+    gic = get_gic()
+    error_count = 0
+    for r in stream:
+        if isinstance(r, StreamHeader):
+            header = r.insert(typename=None, fields=[country_code, region_name, city, latitude, longitude])
+            yield header
+        elif isinstance(r, StreamMeta):
+            yield r
+        else:
+            ip = getattr(r, field)
+            try:
+                if ip in ip_blacklist:
+                    yield header.t(*r + (None, None, None, None, None))
+                    continue
+                cc = gic.record_by_addr(ip)
+                yield header.t(*(r + (cc['country_code'], cc['region_name'], cc['city'], cc['latitude'], cc['longitude'])))
+            except Exception, e:
+                if ignore_error:
+                    if error_count == 0:
+                        print "Error in load", ip, e
+                        error_count = error_count + 1 
+                    yield header.t(*r + (None, None, None, None, None))
+                else:
+                    raise e
+
+BabeBase.register("geoip", geoip)
+
+
 def geoip_country_code(stream, field="ip", country_code="country_code", ignore_error=False, geoip_file=None):
     """"
 Add a 'country_code' field from IP address in field "IP"
