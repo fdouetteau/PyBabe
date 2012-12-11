46cff27620607e108ceca8da016300f5bd93ed10
Gaelle PERIAT <gaelle.periat@iscool-e.com>
2012-12-11 12:58:12 +0100

    2012-12-11 decode params kontagent data

--- pybabe/kontagent.py
+++ pybabe/kontagent.py
@@ -135,11 +135,12 @@ def process_line(base_date, line, discard_names):
                     print e
                     pass
         data = params.get('data', None)
-        data_parameters = base64.b64decode(data)
-        data_object = json.loads(data_parameters)
-        if data_object['recipient'] {
-            recipients = data_object['recipient']
-        } 
+        if data:
+            data_parameters = base64.b64decode(data)
+            data_object = json.loads(data_parameters)
+            if data_object.get('recipient', None):
+                recipients = data_object['recipient']
+         
         if not name:
             name = msgtype
         if name in discard_names:
